6c7675ec0f4bc82d8304c347d55dc61be6009f6a
Alucard2169 <ashish10deepanshu@gmail.com>
2022-03-22 13:44:58 +0530

    back project panel

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ mobMenuButton.addEventListener('change', (e) => {
         menuIcon.alt = 'close Menu'
     } 
 })
+
+
+//back-project menu
+const backProjectMenu = document.querySelector('.back-project');
+const backProjectClosebtn = document.querySelector('#cancel');
+backProjectClosebtn.checked = true;
+if (backProjectClosebtn.checked == true) {
+    backProjectMenu.classList.add('hide')
+}
+
+backProjectClosebtn.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        backProjectMenu.classList.add('hide');
+    }
+    else {
+        backProjectMenu.classList.remove('hide');
+        mobMenuButton.disabled = true;
+    }
+})
+
+// on back-project panel
+
+const backOpenbtns = document.querySelectorAll('.open');
+backOpenbtns.forEach((button) => {
+    button.addEventListener('click', ()=>{
+        backProjectMenu.classList.remove('hide');
+        backProjectClosebtn.checked = false;
+    })
+})
