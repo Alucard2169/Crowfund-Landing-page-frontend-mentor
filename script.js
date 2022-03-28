2387f361db01fbd8804dabea70a0b4363bfd9ea5
Alucard2169 <ashish10deepanshu@gmail.com>
2022-03-28 18:31:43 +0530

    add backpanel design with functionality

--- script.js
+++ script.js
@@ -17,3 +17,54 @@ menuBtn.addEventListener('change', (e) => {
         mobileMenuIcon.src = "images/icon-close-menu.svg";
     }
 })
+
+// back project panel close button
+
+const backClose = document.querySelector("#close");
+const backPanel = document.querySelector('.back-page');
+backPanel.classList.add('display');
+
+
+backClose.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        backPanel.classList.add('display')
+        menuBtn.disabled = false;
+    }
+    else {
+        backPanel.classList.remove('display')
+    }
+})
+
+
+
+// back project select buttons
+
+let backBtn = document.querySelectorAll('input[name="pledge"]');
+let bambooLower = document.querySelector('.bamboo-lower');
+let blackLower = document.querySelector('.black-lower');
+
+backBtn.forEach((button) => {
+    button.addEventListener('change', () => {
+        if (button.value == 'bamboo') {
+            bambooLower.style.display = 'block';
+            blackLower.style.display = 'none';
+        }
+        else if (button.value == 'black') {
+            blackLower.style.display = 'block';
+            bambooLower.style.display = 'none';
+        }
+        else {
+            bambooLower.style.display = 'none';
+            blackLower.style.display = 'none';
+        }
+    })
+})
+
+
+const openBackPanel = document.querySelectorAll('button[value="back-open"]');
+openBackPanel.forEach((button) => {
+    button.addEventListener('click', () => {
+        backPanel.classList.remove('display')
+        menuBtn.disabled = true;
+    })
+})
